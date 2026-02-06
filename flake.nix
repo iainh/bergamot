2ada24d70622024252ee2add61c39137ccbfe653
Iain H <iain@spiralpoint.org>
2026-02-05 20:48:02 -0500

    Add flake.nix with Rust devShell

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "nzbg development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            rustc
+            cargo
+            clippy
+            rustfmt
+            rust-analyzer
+          ];
+
+          RUST_BACKTRACE = 1;
+        };
+      });
+}
